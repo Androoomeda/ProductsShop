482094386df2dd2bfe636f21b52a8fc2c394e81d
Andrey Zatonskiy <zaton.andrey@bk.ru>
2025-08-02 15:41:33 +0700

    Добавлены папки Data, Models, Views, ViewModels
    -Добавлены изображения товаров в папке images
    -Добавлен product.json
    -MainPage.xaml перенесён в папку Views

---
+++ ProductsShop/Models/CartItem.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductsShop.Models
+{
+    internal class CartItem
+    {
+        public Product Product { get; set; }
+
+        public int Quantity;
+
+        public decimal TotalPrice => Product.Price * Quantity;
+    }
+}
---
+++ ProductsShop/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductsShop.Models
+{
+    internal class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+    }
+}
