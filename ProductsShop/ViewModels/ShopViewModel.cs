6b11854367f47fd21be8302ad510eeaa17446198
Andrey Zatonskiy <zaton.andrey@bk.ru>
2025-08-03 20:22:54 +0700

    feat: Добавлена страница магазина с товарами
    -Папка images перенеса в Assets
    -Изменён ImageUri в products.json
    -Добавлен пакет Microsoft.Windows.CsWinRT
    -Добавлен пакет Newronsoft.Json
    -Изменён CartItem. Реализован интерфейс INotifyPropertyChanged для уведомления об изменениях
    -Добавлен ShopViewModel.cs
    -Добавлено навигационное меню в MainPage.xaml
    -Добавлена загрузка страницы магазина и логика выбора меню в MainPage.xaml.cs
    -Добавлена страница ShopPage с выводом товаров

---
+++ ProductsShop/ViewModels/ShopViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using ProductsShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Threading.Tasks;
+
+namespace ProductsShop.ViewModels
+{
+    public class ShopViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<Product> Products { get; } = new();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public async Task LoadProductsAsync()
+        {
+            var file = await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFileAsync(@"Data\products.json");
+            var json = await Windows.Storage.FileIO.ReadTextAsync(file);
+            var products = JsonConvert.DeserializeObject<List<Product>>(json);
+            Products.Clear();
+            foreach (var p in products)
+                Products.Add(p);
+        }
+    }
+}
